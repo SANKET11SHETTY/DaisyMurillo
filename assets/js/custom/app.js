097090360b08a6d4fe05028a883ee5e93b73d30e
SANKET SHETTY <legendhimself007@gmail.com>
2025-11-28 11:49:31 +0530

    Update app.js

--- assets/js/custom/app.js
+++ assets/js/custom/app.js
@@ -91,4 +91,17 @@ $(window).on("scroll counter", function () {
     });
   }
 });
-
+// Form Validation
+(() => {
+  'use strict'
+  const forms = document.querySelectorAll('.needs-validation')
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
